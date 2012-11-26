d9840ae8b5e0eb736a6c39924e9da6f781a4e00f
Niv Sardi <xaiki@evilgiggle.com>
2012-11-26 09:58:59 -0300

    port diffs from backbone.iobind
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- backboneio.js
+++ backboneio.js
@@ -24,15 +24,21 @@
 
   BackboneIO.sync = (function (method, model, options) {
     var getUrl = function (object) {
+      if (options && options.url) {
+        return _.isFunction(options.url) ? options.url() : options.url;
+      }
+
       if (!(object && object.url)) return null;
       return _.isFunction(object.url) ? object.url() : object.url;
     };
-    
+
     options = options || {};
     var url = options.url || getUrl(model);
+    var cmd = url.split('/')
+    , namespace = (cmd[0] !== '') ? cmd[0] : cmd[1]; // if leading slash, ignore
 
     var params = _.extend({
-      req: url + ':' + method
+      req: namespace + ':' + method
     }, options);
 
     if ( !params.data && model ) {
@@ -57,6 +63,137 @@
       });
     }
   });
+
+/**
+ * # .ioBind(event, callback, [context])
+ *
+ * Bind and handle trigger of socket.io events for models.
+ *
+ * ### Guidelines
+ *
+ * Do NOT bind to reserved backbone events, such as `change`, `remove`, and `add`.
+ * Proxy these events using different event tags such as `update`, `delete`, and `create`.
+ *
+ * The socket.io socket must either exist at `window.socket`, `Backbone.socket`, or
+ * `this.socket` or it must be passed as the second argument.
+ *
+ * ### Example
+ *
+ * * Model definition has url: `my_model`
+ * * Model instance has id: `abc123`
+ *
+ * #### Create a new bind (client-side):
+ *
+ *     model.ioBind('update', window.io, this.updateView, this);
+ *
+ * #### Send socket.io message (server-side)
+ *
+ *     socket.emit( 'my_model/abc123:update', { title: 'My New Title' } );
+ *
+ * @name ioBind
+ * @param {String} eventName
+ * @param {Object} io from active socket.io connection (optional)
+ * @param {Function} callback
+ * @param {Object} context (optional) object to interpret as this on callback
+ * @api public
+ */
+
+    Backbone.Model.prototype.ioBind = Backbone.Collection.prototype.ioBind =  function (eventName, io, callback, context) {
+      var url = (typeof this.url == 'function') ? this.url() : this.url;
+      var ioEvents = this._ioEvents || (this._ioEvents = {})
+        , globalName = url + ':' + eventName
+        , self = this;
+      if ('function' == typeof io) {
+        context = callback;
+        callback = io;
+        io = this.socket || window.socket || Backbone.socket;
+      }
+      var event = {
+        name: eventName,
+        global: globalName,
+        cbLocal: callback,
+        cbGlobal: function (data) {
+            var args = [eventName];
+            args.push.apply(args, arguments);
+            self.trigger.apply(self, args);
+        }
+      };
+      this.bind(event.name, event.cbLocal, (context || self));
+      io.on(event.global, event.cbGlobal);
+      if (!ioEvents[event.name]) {
+        ioEvents[event.name] = [event];
+      } else {
+        ioEvents[event.name].push(event);
+      }
+      return this;
+    },
+
+/**
+ * # .ioUnbind(event, [callback])
+ *
+ * Unbind model triggers and stop listening for server events for a specific
+ * event and optional callback.
+ *
+ * The socket.io socket must either exist at `window.socket`, `Backbone.socket`,
+ * or `this.socket` or it must be passed as the second argument.
+ *
+ * @name ioUnbind
+ * @param {String} eventName
+ * @param {Object} io from active socket.io connection
+ * @param {Function} callback (optional) If not provided will remove all callbacks for eventname.
+ * @api public
+ */
+
+Backbone.Model.prototype.ioUnbind = Backbone.Collection.prototype.ioUnbind = function (eventName, io, callback) {
+  var ioEvents = this._ioEvents || (this._ioEvents = {})
+    , globalName = this.url() + ':' + eventName;
+  if ('function' == typeof io) {
+    callback = io;
+    io = this.socket || window.socket || Backbone.socket;
+  }
+  var events = ioEvents[eventName];
+  if (!_.isEmpty(events)) {
+    if (callback && 'function' === typeof callback) {
+      for (var i = 0, l = events.length; i < l; i++) {
+        if (callback == events[i].cbLocal) {
+          this.unbind(events[i].name, events[i].cbLocal);
+          io.removeListener(events[i].global, events[i].cbGlobal);
+          events[i] = false;
+        }
+      }
+      events = _.compact(events);
+    } else {
+      this.unbind(eventName);
+      io.removeAllListeners(globalName);
+    }
+    if (events.length === 0) {
+      delete ioEvents[eventName];
+    }
+  }
+  return this;
+};
+
+/**
+ * # .ioUnbindAll()
+ *
+ * Unbind all callbacks and server listening events for the given model.
+ *
+ * The socket.io socket must either exist at `window.socket`, `Backbone.socket`,
+ * or `this.socket` or it must be passed as the only argument.
+ *
+ * @name ioUnbindAll
+ * @param {Object} io from active socket.io connection
+ * @api public
+ */
+
+Backbone.Model.prototype.ioUnbindAll = Backbone.Collection.prototype.ioUnbindAll = function (io) {
+  var ioEvents = this._ioEvents || (this._ioEvents = {});
+  if (!io) io = this.socket || window.socket || Backbone.socket;
+  for (var ev in ioEvents) {
+    this.ioUnbind(ev, io);
+  }
+  return this;
+};
 
   BackboneIO.Model = Backbone.Model.extend({
     sockets: {},
@@ -112,68 +249,6 @@
       this.ioUnbindAll(io);
       return this;
     },
-    ioBind: function (eventName, io, callback, context) {
-      var ioEvents = this._ioEvents || (this._ioEvents = {})
-        , globalName = this.url() + ':' + eventName
-        , self = this;
-      if ('function' == typeof io) {
-        context = callback;
-        callback = io;
-        io = this.socket || window.socket || Backbone.socket;
-      }
-      var event = {
-        name: eventName,
-        global: globalName,
-        cbLocal: callback,
-        cbGlobal: function (data) {
-          self.trigger(eventName, data);
-        }
-      };
-      this.bind(event.name, event.cbLocal, (context || self));
-      io.on(event.global, event.cbGlobal);
-      if (!ioEvents[event.name]) {
-        ioEvents[event.name] = [event];
-      } else {
-        ioEvents[event.name].push(event);
-      }
-      return this;
-    },
-    ioUnbind: function (eventName, io, callback) {
-      var ioEvents = this._ioEvents || (this._ioEvents = {})
-        , globalName = this.url() + ':' + eventName;
-      if ('function' == typeof io) {
-        callback = io;
-        io = this.socket || window.socket || Backbone.socket;
-      }
-      var events = ioEvents[eventName];
-      if (!_.isEmpty(events)) {
-        if (callback && 'function' === typeof callback) {
-          for (var i = 0, l = events.length; i < l; i++) {
-            if (callback == events[i].cbLocal) {
-              this.unbind(events[i].name, events[i].cbLocal);
-              io.removeListener(events[i].global, events[i].cbGlobal);
-              events[i] = false;
-            }
-          }
-          events = _.compact(events);
-        } else {
-          this.unbind(eventName);
-          io.removeAllListeners(globalName);
-        }
-        if (events.length === 0) {
-          delete ioEvents[eventName];
-        }
-      }
-      return this;
-    },
-    ioUnbindAll: function (io) {
-      var ioEvents = this._ioEvents || (this._ioEvents = {});
-      if (!io) io = this.socket || window.socket || Backbone.socket;
-      for (var ev in ioEvents) {
-        this.ioUnbind(ev, io);
-      }
-      return this;
-    },
     sync: BackboneIO.sync,
   });
 
@@ -185,9 +260,9 @@
       if(typeof this.id === 'undefined') this.id = this.cid;
       _.bindAll(this, 'onClientRead','onClientCreate', 'collectionCleanup');
       if (!this.noIoBind) {
+          console.log ('binding on', this.url);
         // Hack, because it create duplicated entries
-        //this.ioBind('create', socket, this.onClientCreate, this);
-        socket.on(this.url+':create', this.onClientCreate);
+          socket.on(this.url+':create', this.onClientCreate);
         // Hack, because with iobind the callback function is undefined
         //this.ioBind('read', socket, this.onClientRead, this);
         socket.on(this.url+':read', this.onClientRead);
@@ -206,6 +281,7 @@
       fn(null,this.toJSON());
     },
     onClientCreate: function(data) {
+        console.log ('create');
       var exists = this.get(data.id);
       if (!exists) {
         exists = new this.model(data);
@@ -223,6 +299,7 @@
       _.bindAll(this, 'onServerCreate', 'collectionCleanup');
       if (!this.noIoBind) {
         this.ioBind('create', this.onServerCreate, this);
+        this.ioBind('update', this.onServerCreate, this);
       }
       this.each(function(model) {
         model.bindClient();
@@ -244,68 +321,6 @@
       this.each(function (model) {
         model.modelCleanup();
       });
-      return this;
-    },
-    ioBind: function (eventName, io, callback, context) {
-      var ioEvents = this._ioEvents || (this._ioEvents = {})
-        , globalName = this.url + ':' + eventName
-        , self = this;
-      if ('function' == typeof io) {
-        context = callback;
-        callback = io;
-        io = this.socket || window.socket || Backbone.socket;
-      }
-      var event = {
-        name: eventName,
-        global: globalName,
-        cbLocal: callback,
-        cbGlobal: function (data) {
-          self.trigger(eventName, data);
-        }
-      };
-      this.bind(event.name, event.cbLocal, context);
-      io.on(event.global, event.cbGlobal);
-      if (!ioEvents[event.name]) {
-        ioEvents[event.name] = [event];
-      } else {
-        ioEvents[event.name].push(event);
-      }
-      return this;
-    }, 
-    ioUnbind: function (eventName, io, callback) {
-      var ioEvents = this._ioEvents || (this._ioEvents = {})
-        , globalName = this.url + ':' + eventName;
-      if ('function' == typeof io) {
-        callback = io;
-        io = this.socket || window.socket || Backbone.socket;
-      }
-      var events = ioEvents[eventName];
-      if (!_.isEmpty(events)) {
-        if (callback && 'function' === typeof callback) {
-          for (var i = 0, l = events.length; i < l; i++) {
-            if (callback == events[i].cbLocal) {
-              this.unbind(events[i].name, events[i].cbLocal);
-              io.removeListener(events[i].global, events[i].cbGlobal);
-              events[i] = false;
-            }
-          }
-          events = _.compact(events);
-        } else {
-          this.unbind(eventName);
-          io.removeAllListeners(globalName);
-        }
-        if (events.length === 0) {
-          delete ioEvents[eventName];
-        }
-      }
-      return this;
-    },
-    ioUnbindAll: function (io) {
-      var ioEvents = this._ioEvents || (this._ioEvents = {});
-      if (!io) io = this.socket || window.socket || Backbone.socket;
-      for (var ev in ioEvents) {
-        this.ioUnbind(ev, io);
-      }
       return this;
     },
     sync: BackboneIO.sync,
